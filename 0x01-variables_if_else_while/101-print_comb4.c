5d4b14a218f5f034fbc6af6aedff2f853f181d20
Praeiz <mabeokwupraise@gmail.com>
2022-12-12 16:37:13 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,18 +13,18 @@ int main(void)
 
 	for (hundreds = '0'; hundreds <= '9'; hundreds++)
 	{
-		for (tens ='0'; tens <= '9'; tens++)
+		for (tens = '0'; tens <= '9'; tens++)
 		{
-			for ones = '0';ones <= '9'; ones++)
+			for ones = '0'; ones <= '9'; ones++)
 			{
 				if (!((ones == tens) || (tens == hundreds) ||
-							(tens > ones) || (hundreds > tens)))/*eliminates repitition*/
+							(tens > ones) || (hundreds > tens))) /*eliminates repitition*/
 				{
 					putchar(hundreds);
 					putchar(teens);
 					putchar(ones);
 					if (!(ones == '9' && hundreds == '7' &&
-								tens == '8'))adds commas and spaces*/
+								tens == '8')) /*adds commas and spaces*/
 					{
 						putchar(',');
 						putchar(' ');
