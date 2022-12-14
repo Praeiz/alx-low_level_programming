67c1c56ec5fa1d4336fd5bee31c274c71e0bfa5a
Praeiz <mabeokwupraise@gmail.com>
2022-12-14 03:45:58 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase letter
+ * @c: character to check the caase
+ * Return: 1 and 0
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
+
--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main . check the code.
+ * main - check the code.
  *
  * Return: Always 0.
  */
@@ -13,7 +13,7 @@ int main(void)
 	r = _islower('H');
 	_putchar(r + '0');
 	r = _islower('o');
-	_putchar(r +'0');
+	_putchar(r + '0');
 	r = _islower(108);
 	_putchar(r + '0');
 	_putchar('\n');
