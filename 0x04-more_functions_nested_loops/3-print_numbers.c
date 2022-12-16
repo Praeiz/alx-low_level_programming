b87fbfe177e7fdf10764fe9c0c7a16af3de026da
Praeiz <mabeokwupraise@gmail.com>
2022-12-15 18:58:40 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,11 @@
 
 void print_numbers(void)
 {
-	char c;
+	int n;
 
-	for (c = '0'; c <= '9'; c++)
+	for (n = 48; n < 58; n++)
 	{
-		_putchar(c);
+		putchar (n);
 	}
-	_putchar('\n');
+	putchar(10);
 }
-
